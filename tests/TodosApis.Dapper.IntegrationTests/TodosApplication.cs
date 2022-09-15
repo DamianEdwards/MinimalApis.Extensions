2ef5b674b7e6565b5e4ef2b28b5ae0eeba9c25b7
Damian Edwards <damian@damianedwards.com>
2022-09-14 18:14:51 -0700

    Remove unused dep in test project

--- tests/TodosApis.Dapper.IntegrationTests/TodosApplication.cs
+++ tests/TodosApis.Dapper.IntegrationTests/TodosApplication.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Dapper;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TodosApi.Dapper;
